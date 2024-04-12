42d7c5ae38676105c9788e32e23645b237605521
devnahidislam <mdnahidislam30@gmail.com>
2024-04-12 11:14:18 +0600

    First route setup.

--- src/app.js
+++ src/app.js
@@ -17,4 +17,12 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+// Routes import
+import userRouter from "./routes/user.routes.js";
+
+// Routes Descaration
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
+
 export { app };
